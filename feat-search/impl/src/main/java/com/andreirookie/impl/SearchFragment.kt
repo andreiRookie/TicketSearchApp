4e38b2420c95edd34c249de7d337b115a0f6ea0a
AndreiRookie <andrey.idem@gmail.com>
2024-06-27 12:36:10 +0400

    search screen header added

--- feat-search/impl/src/main/java/com/andreirookie/impl/SearchFragment.kt
+++ feat-search/impl/src/main/java/com/andreirookie/impl/SearchFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 
 class SearchFragment : Fragment() {
+
+    private lateinit var fromEdittext: EditText
+    private lateinit var toEdittext: EditText
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -14,5 +18,15 @@ class SearchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.search_fragment_layout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fromEdittext = view.findViewById(com.andreirookie.uikit.R.id.from_edittext)
+        toEdittext = view.findViewById(com.andreirookie.uikit.R.id.to_edittext)
+
+        fromEdittext.hint = getString(R.string.search_screen_from_edittext_hint)
+        toEdittext.hint = getString(R.string.search_screen_to_edittext_hint)
     }
 }
