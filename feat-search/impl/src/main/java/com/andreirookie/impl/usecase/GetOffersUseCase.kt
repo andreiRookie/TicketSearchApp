c3ca8b5167dd87679e091bb3fc80ce4e8cff870a
AndreiRookie <andrey.idem@gmail.com>
2024-08-17 17:21:31 +0400

    GetOffersUseCAse commited

---
+++ feat-search/impl/src/main/java/com/andreirookie/impl/usecase/GetOffersUseCase.kt
@@ -0,0 +1,34 @@
+package com.andreirookie.impl.usecase
+
+import com.andreirookie.impl.DispatchersGetter
+import com.andreirookie.impl.di.Api
+import com.andreirookie.impl.di.Stub
+import com.andreirookie.impl.reposirory.OfferItemModel
+import com.andreirookie.impl.reposirory.OffersRepository
+import kotlinx.coroutines.withContext
+import java.util.concurrent.CancellationException
+import javax.inject.Inject
+
+interface GetOffersUseCase {
+    suspend operator fun invoke(): List<OfferItemModel>
+}
+
+class GetOffersUseCaseImpl @Inject constructor(
+    @Api
+    private val apiRepository: OffersRepository,
+    @Stub
+    private val stubRepository: OffersRepository,
+    private val dispatchers: DispatchersGetter
+) : GetOffersUseCase {
+    override suspend operator fun invoke(): List<OfferItemModel> {
+        return withContext(dispatchers.default) {
+            try {
+                apiRepository.getOffers()
+            } catch (e: CancellationException) {
+                throw e
+            } catch (e: Exception) {
+                stubRepository.getOffers()
+            }
+        }
+    }
+}
